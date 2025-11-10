198a6a479c0cf51e4290b33fb004ca04145e0f9a
Kaustav Roy <freakyray002@gmail.com>
2025-11-10 20:11:50 +0530

    Type Conversion done and added

---
+++ 1_basics/conversionOperation.js
@@ -0,0 +1,51 @@
+let score = 33; // number type data
+let score1 = "33abc"; // string type data
+let score2 = null;
+let score3 = true;
+
+console.log(typeof score);
+console.log(typeof score1);
+
+// type conversion
+// from All to Number
+let valueInNumber = Number(score1);
+let valueUpdate = Number(score2);
+let valueUpdate2 = Number(score3);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber); //return NaN aka Not a Number
+console.log(valueUpdate);
+console.log(valueUpdate2);
+
+/* 33 gets easily converted into 33
+    "33abs" => NaN
+    true => 1 ; false => 0
+*/
+
+// from All to Boolean
+let isLoggedIn = 1;
+let isLoggedOut = 0;
+let isLoggedIn2 = "";
+let isLoggedIn3 = "Kaustav";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2);
+let booleanIsLoggedIn3 = Boolean(isLoggedIn3);
+let booleanIsLoggedOut = Boolean(isLoggedOut);
+
+console.log(booleanIsLoggedIn); //returns true
+console.log(booleanIsLoggedIn2); //returns false
+console.log(booleanIsLoggedIn3); //returns true
+console.log(booleanIsLoggedOut); //returns false
+
+/* 1 => true, 0 => false
+    "" => false
+    "Kaustav" => true */
+
+// form All to String
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
