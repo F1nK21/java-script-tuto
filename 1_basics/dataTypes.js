8fd652391210698a4327cf2601d64ad4a5305d63
Kaustav Roy <freakyray002@gmail.com>
2025-11-10 18:56:58 +0530

    now file added

---
+++ 1_basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3); //we are using node.js not browser
+
+//Data Types
+let name = "Kaustav"; //String type data
+let age = 23; // integer or number type data
+let isLoggedIn = true; //boolean type data
+
+//Premitive Data Types
+//number => 2 to the power 53
+//bigint
+//String => ""
+//boolean => returns true or false
+//null => stand alone value
+//undefined => value to a variable is not yet defined
+//symbol => unique
+
+//objects yet to discuss
+
+console.log(typeof name); // returns string
+console.log(typeof null); // returns object
+console.log(typeof undefined); // returns undefined
+console.log(typeof age); // returns number
+console.log(typeof isLoggedIn); // returns boolean
